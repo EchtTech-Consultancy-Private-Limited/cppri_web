3a08d9d96def174abffde344af4d7d8fc4668c4d
neeraj7355 <neeraj.chaursiya@echttech.com>
2024-01-09 18:42:10 +0530

    worked on themem mode

--- assets-cppri/js/swithcer.js
+++ assets-cppri/js/swithcer.js
@@ -141,4 +141,46 @@ jQuery(document).ready(function(){
 		
 		
 	}
+
+
+	// blue
+	jQuery('.blue').click(function(){	
+		var thirtyDays = 1000*60*60*24*30;
+		var expireDate = new Date((new Date()).valueOf() + thirtyDays);		
+		document.cookie = 'contrast' +"="+ 1 +"; expires="+expireDate.toGMTString()+"; path=/";
+		document.cookie="username=John Doe";
+		jQuery('head').append("<link rel='stylesheet' type='text/css' media='screen' href='"+baseUrl1+"/assets-cppri/css/blue.css'>");
+		
+		
+	});
+	// purple
+	jQuery('.purple').click(function(){	
+		var thirtyDays = 1000*60*60*24*30;
+		var expireDate = new Date((new Date()).valueOf() + thirtyDays);		
+		document.cookie = 'contrast' +"="+ 1 +"; expires="+expireDate.toGMTString()+"; path=/";
+		document.cookie="username=John Doe";
+		jQuery('head').append("<link rel='stylesheet' type='text/css' media='screen' href='"+baseUrl1+"/assets-cppri/css/purple.css'>");
+		
+		
+	});
+	// orange
+	jQuery('.orange').click(function(){	
+		var thirtyDays = 1000*60*60*24*30;
+		var expireDate = new Date((new Date()).valueOf() + thirtyDays);		
+		document.cookie = 'contrast' +"="+ 1 +"; expires="+expireDate.toGMTString()+"; path=/";
+		document.cookie="username=John Doe";
+		jQuery('head').append("<link rel='stylesheet' type='text/css' media='screen' href='"+baseUrl1+"/assets-cppri/css/orange.css'>");
+		
+		
+	});
+	// green
+	jQuery('.green').click(function(){	
+		var thirtyDays = 1000*60*60*24*30;
+		var expireDate = new Date((new Date()).valueOf() + thirtyDays);		
+		document.cookie = 'contrast' +"="+ 1 +"; expires="+expireDate.toGMTString()+"; path=/";
+		document.cookie="username=John Doe";
+		jQuery('head').append("<link rel='stylesheet' type='text/css' media='screen' href='"+baseUrl1+"/assets-cppri/css/green.css'>");
+		
+		
+	});
 });
