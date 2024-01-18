472086c00a1e7a3e24f63c3baeba22da27d021c7
neeraj7355 <neeraj.chaursiya@echttech.com>
2024-01-18 16:45:58 +0530

    fixed sidebar

--- assets-cppri/custom.js
+++ assets-cppri/custom.js
@@ -498,8 +498,37 @@ $(document).ready(function() {
            
         // })
 
-        let test = document.querySelectorAll('.accordion-collapse .accordion-body ul li');
+       
+        let test2 = document.querySelectorAll('.accordion-collapse .accordion-body ul li ul li');
 
+        test2.forEach((e) => {
+            if (e.classList.contains('qm-active')) {
+                let currentElementId = e.closest('.accordion').id;
+                console.log('sal;dkfj',  e.closest('.accordion'))
+                let mainParent =  e.closest('.accordion').parentElement.parentElement.parentElement.classList.add('show')
+                console.log('mainparent', mainParent)
+                let menu_active = document.getElementById(currentElementId).classList.add('menu-active')
+                console.log('menu-active', menu_active)
+                console.log(currentElementId);
+                let accordion = e.closest('.accordion');
+                let currentElement = document.getElementById(currentElementId);
+                console.log(currentElement.firstElementChild.childNodes[3])
+                let addClass = currentElement.firstElementChild.childNodes[3];
+                console.log('fl', addClass);
+        
+                if (addClass) {
+                    addClass.classList.add('show');
+                }
+        
+                let mainParentElement =currentElement.firstElementChild.childNodes[1].childNodes[1];
+                console.log(mainParentElement)
+                if (mainParentElement) {
+                    mainParentElement.classList.add('collapsed');
+                }
+            }
+        });
+
+        let test = document.querySelectorAll('.accordion-collapse .accordion-body ul li');
 test.forEach((e) => {
     if (e.classList.contains('qm-active')) {
         let currentElementId = e.closest('.accordion').id;
@@ -572,14 +601,14 @@ $('.preventnumeric').keypress(function(e) {
 //     })
 // });
 $(document).ready(function() {
-    let sl_accordion = $('.sl-accordion');
+    // let sl_accordion = $('.sl-accordion');
     
-    sl_accordion.click(function() {
-       sl_accordion.removeClass('menu-accordion-click');
-        $(this).addClass('menu-accordion-click');
-        // alert('click');
+    // sl_accordion.click(function() {
+    //    sl_accordion.removeClass('menu-accordion-click');
+    //     $(this).addClass('menu-accordion-click');
+    //     // alert('click');
 
-    });
+    // });
     let fl_accordion = $('.fl-accordion');
    
 
