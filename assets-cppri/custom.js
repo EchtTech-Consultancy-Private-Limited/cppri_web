d4ca0abf90142342a5f42de17d2e55e9fc828773
neeraj7355 <neeraj.chaursiya@echttech.com>
2024-02-07 14:34:13 +0530

    photo gallery

--- assets-cppri/custom.js
+++ assets-cppri/custom.js
@@ -771,13 +771,13 @@ function myFunction() {
                     center: false,
                 },
                 768: {
-                    items: (smDevice ? smDevice : 3),
+                    items: (smDevice ? smDevice : 2),
                     nav: (smDeviceNav ? true : false),
                     dots: (smDeviceDots ? true : false),
                     center: false,
                 },
                 992: {
-                    items: (mdDevice ? mdDevice : 4),
+                    items: (mdDevice ? mdDevice : 2),
                     nav: (mdDeviceNav ? true : false),
                     dots: (mdDeviceDots ? true : false),
                 }
@@ -864,3 +864,31 @@ play4.addEventListener("click", function () {
     play4.style.display = "none";
     pause4.style.display = "block"
 });
+
+// detail page gallery js
+
+let slideIndex = 1;
+showSlides(slideIndex);        
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}        
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}        
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
