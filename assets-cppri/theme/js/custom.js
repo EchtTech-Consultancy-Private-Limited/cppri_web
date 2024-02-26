33e901489b0eefa0e767580a1dc8dae16226f5e6
Echt Tech <rahul.chaurasia@echttech.com>
2024-02-26 11:53:26 +0530

    add_faqs

--- assets-cppri/theme/js/custom.js
+++ assets-cppri/theme/js/custom.js
@@ -340,3 +340,22 @@ function topFunction() {
             n < 1 && window.requestAnimationFrame(o);
     });
 }
+
+// accordion js
+
+var acc = document.getElementsByClassName("contentAccordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+// End accordion js
